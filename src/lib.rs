04274238d16a705305b69c685069dcaf4126892d
Lucas Holten <lhc.holten@gmail.com>
2024-03-18 15:55:53 +0100

    Refactor ast

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use std::{
     ops::{Deref, DerefMut},
 };
 
-use ast::{MyDef, MySelect, MyTable, Operation};
+use ast::{MyDef, MySelect, Operation, Source};
 use sea_query::{Alias, SelectStatement};
 use value::{MyIden, Value};
 
@@ -29,17 +29,17 @@ pub trait Table {
 }
 
 impl<'names> Query<'names> {
-    pub fn join<T: Table>(&mut self, _t: T) -> T::Dummy<'names> {
+    pub fn table<T: Table>(&mut self, _t: T) -> T::Dummy<'names> {
         let mut columns = Vec::new();
         let res = T::build(|name| {
             let alias = MyAlias::new();
-            columns.push((Alias::new(name), alias));
+            columns.push((name, alias));
             alias.iden()
         });
-        self.ast.0.push(Operation::From(MyTable::Def(MyDef {
-            table: Alias::new(T::NAME),
+        self.ast.sources.push(Source::Table(MyDef {
+            table: T::NAME,
             columns,
-        })));
+        }));
         res
     }
 
@@ -48,20 +48,18 @@ impl<'names> Query<'names> {
     where
         F: for<'a> FnOnce(Group<'a, 'names>),
     {
-        let mut ast = MySelect(vec![]);
+        let mut ast = MySelect::default();
         let inner = Query {
             names: PhantomData,
             ast: &mut ast,
         };
         let group = Group { outer: self, inner };
         f(group);
-        self.ast
-            .0
-            .push(ast::Operation::From(ast::MyTable::Select(ast)))
+        self.ast.sources.push(ast::Source::Select(ast))
     }
 
     pub fn filter(&mut self, prop: impl Value + 'names) {
-        self.ast.0.push(ast::Operation::Filter(prop.into_expr()));
+        self.ast.filters.push(prop.into_expr());
     }
 }
 
@@ -85,7 +83,8 @@ impl<'a, 'names> DerefMut for Group<'a, 'names> {
 }
 
 impl<'a, 'names> Group<'a, 'names> {
-    fn by(self, val: impl Value + 'a) -> Aggr<'a, 'names> {
+    fn group(mut self, val: impl Value + 'a) -> Aggr<'a, 'names> {
+        self.ast.group = Some(val.into_expr());
         Aggr { group: self }
     }
 }
@@ -116,7 +115,7 @@ pub fn new_query<F>(f: F) -> SelectStatement
 where
     F: for<'a> FnOnce(Query<'a>),
 {
-    let mut inner = MySelect(vec![]);
+    let mut inner = MySelect::default();
     let query = Query {
         names: PhantomData,
         ast: &mut inner,
@@ -153,12 +152,12 @@ mod tests {
     #[test]
     fn test() {
         new_query(|mut q| {
-            let q_test = q.join(TestTable);
+            let q_test = q.table(TestTable);
             let mut out = None;
             q.query(|mut g| {
-                let g_test = g.join(TestTable);
+                let g_test = g.table(TestTable);
                 g.filter(q_test.foo);
-                let mut aggr = g.by(g_test.foo);
+                let mut aggr = g.group(g_test.foo);
                 out = Some(aggr.average(g_test.foo));
             });
             q.filter(out.unwrap());
@@ -168,25 +167,25 @@ mod tests {
             //     // q.filter(test_p.foo);
             //     // p.filter(test_q.foo);
             // });
-            let test_q = q.join(TestTable);
+            let test_q = q.table(TestTable);
             // let x = sub_query(&mut q, test_q.foo);
             // q.filter(x);
         });
     }
 
     fn get_match<'a>(q: &mut Query<'a>, foo: impl Value + 'a) -> impl Value + 'a {
-        let test = q.join(TestTable);
+        let test = q.table(TestTable);
         q.filter(test.foo.eq(foo));
         test.foo
     }
 
     fn transpose() {
         new_query(|mut q| {
-            let alpha = q.join(TestTable);
+            let alpha = q.table(TestTable);
             let mut beta = None;
             q.query(|mut g| {
                 let res = get_match(&mut g, alpha.foo);
-                let mut res = g.by(res);
+                let mut res = g.group(res);
                 beta = Some(res.rank_asc(alpha.foo));
             });
             q.filter(alpha.foo.eq(beta.unwrap()))
