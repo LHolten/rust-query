ce46dc175025b23cbb2cd15a42c997fb25caebda
Hytak <lhc.holten@gmail.com>
2024-09-02 14:12:51 +0200

    Fix schema macro use of NoTable

--- rust-query-macros/src/lib.rs
+++ rust-query-macros/src/lib.rs
@@ -551,7 +551,7 @@ fn prelude(new_tables: &BTreeMap<usize, Table>, prev_mod: &Ident, schema: &Ident
             let new_name = &table.name;
             prelude.extend(quote! {
                 #[allow(unused_imports)]
-                use ::rust_query::NoTable as #new_name;
+                use ::rust_query::migration::NoTable as #new_name;
             })
         }
     }
