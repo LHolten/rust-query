e05ed4a44cc9bdc2be7be0b44d5723a3ef7c4f96
lucasholten <lucas@lucasholten.com>
2025-09-08 20:16:08 +0200

    Update alias Into<DynIden> impls

--- src/alias.rs
+++ src/alias.rs
@@ -47,24 +47,24 @@ pub(super) struct TmpTable {
     name: u64,
 }
 
-impl sea_query::IntoIden for Field {
-    fn into_iden(self) -> sea_query::DynIden {
-        match self {
-            Field::U64(alias) => alias.into_iden(),
-            Field::Str(name) => name.into_iden(),
+impl From<Field> for sea_query::DynIden {
+    fn from(value: Field) -> Self {
+        match value {
+            Field::U64(alias) => alias.into(),
+            Field::Str(name) => name.into(),
         }
     }
 }
 
-impl sea_query::IntoIden for MyAlias {
-    fn into_iden(self) -> sea_query::DynIden {
-        format!("_{}", self.name).into_iden()
+impl From<MyAlias> for sea_query::DynIden {
+    fn from(value: MyAlias) -> Self {
+        format!("_{}", value.name).into()
     }
 }
 
-impl sea_query::IntoIden for TmpTable {
-    fn into_iden(self) -> sea_query::DynIden {
-        format!("_tmp{}", self.name).into_iden()
+impl From<TmpTable> for sea_query::DynIden {
+    fn from(value: TmpTable) -> Self {
+        format!("_tmp{}", value.name).into()
     }
 }
 
