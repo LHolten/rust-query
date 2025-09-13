1c4928015a1d896128f470c6105a323d09e69823
lucasholten <lucas@lucasholten.com>
2025-09-13 15:37:01 +0200

    Add test for deleting referenced table

--- examples/empty_table.rs
+++ examples/empty_table.rs
@@ -6,6 +6,10 @@ use rust_query::{
 #[schema(Schema)]
 pub mod vN {
     pub struct Empty;
+    #[no_reference]
+    pub struct Ref {
+        pub empty: Empty,
+    }
 }
 
 pub fn main() {
@@ -15,9 +19,14 @@ pub fn main() {
         .expect("database is newer than supported versions");
 
     db.transaction_mut_ok(|txn| {
-        let id = txn.insert(v0::Empty).unwrap();
+        let id = txn.insert_ok(v0::Empty);
+        let id2 = txn.insert_ok(v0::Empty);
+        let r = txn.insert_ok(v0::Ref { empty: id2 });
         let id = txn.query_one(id.into_expr());
         let txn = txn.downgrade();
         assert!(txn.delete(id).unwrap());
+        txn.delete(id2).unwrap_err();
+        txn.delete_ok(r);
+        assert!(txn.delete(id2).unwrap());
     })
 }
