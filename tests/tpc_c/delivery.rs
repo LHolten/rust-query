09ad6e37603a502fa3f4789f3d0e4217ee304fe7
lucasholten <lucas@lucasholten.com>
2025-09-20 18:45:40 +0200

    Update emulated_user.rs

--- tests/tpc_c/delivery.rs
+++ tests/tpc_c/delivery.rs
@@ -1,14 +1,14 @@
 use std::time::UNIX_EPOCH;
 
-use rust_query::{Transaction, Update, aggregate, optional};
+use rust_query::{Update, aggregate, optional};
 
 use super::*;
 
-pub fn random_delivery(txn: &'static mut Transaction<Schema>, warehouse: TableRow<Warehouse>) {
-    delivery(txn, generate_input(warehouse));
+pub fn random_delivery(db: &Database<Schema>, warehouse: i64) {
+    delivery(db, generate_input(warehouse));
 }
 
-fn generate_input(warehouse: TableRow<Warehouse>) -> DeliveryInput {
+fn generate_input(warehouse: i64) -> DeliveryInput {
     DeliveryInput {
         warehouse,
         carrier_id: rand::random_range(1..=10),
@@ -17,77 +17,78 @@ fn generate_input(warehouse: TableRow<Warehouse>) -> DeliveryInput {
 }
 
 struct DeliveryInput {
-    warehouse: TableRow<Warehouse>,
+    warehouse: i64,
     carrier_id: i64,
     delivery_d: i64,
 }
 
-fn delivery(txn: &'static mut Transaction<Schema>, input: DeliveryInput) {
-    let mut new_orders = vec![];
+fn delivery(db: &Database<Schema>, input: DeliveryInput) {
     for district_num in 1..=10 {
-        let district = txn
-            .query_one(District::unique(input.warehouse, district_num))
-            .unwrap();
+        // use separate transactions to allow other threads to do stuff in between
+        db.transaction_mut_ok(|txn| {
+            let warehouse = txn.query_one(Warehouse::unique(input.warehouse)).unwrap();
+            let district = txn
+                .query_one(District::unique(warehouse, district_num))
+                .unwrap();
 
-        let new_order = txn.query_one(optional(|row| {
-            aggregate(|rows| {
-                let new_order = rows.join(NewOrder);
-                let order = &new_order.order;
-                let customer = &order.customer;
-                rows.filter(customer.district.eq(district));
+            let new_order = txn.query_one(optional(|row| {
+                aggregate(|rows| {
+                    let new_order = rows.join(NewOrder);
+                    let order = &new_order.order;
+                    let customer = &order.customer;
+                    rows.filter(customer.district.eq(district));
 
-                let order_num = row.and(rows.min(&order.number));
-                rows.filter(order.number.eq(&order_num));
+                    let order_num = row.and(rows.min(&order.number));
+                    rows.filter(order.number.eq(&order_num));
 
-                let customer_num = row.and(rows.min(&customer.number));
-                let customer = row.and(Customer::unique(district, customer_num));
-                let order = row.and(Order::unique(customer, order_num));
-                let new_order = row.and(NewOrder::unique(order));
-                row.then(new_order)
-            })
-        }));
-        let Some(new_order) = new_order else {
-            continue;
-        };
+                    let customer_num = row.and(rows.min(&customer.number));
+                    let customer = row.and(Customer::unique(district, customer_num));
+                    let order = row.and(Order::unique(customer, order_num));
+                    let new_order = row.and(NewOrder::unique(order));
+                    row.then(new_order)
+                })
+            }));
+            let Some(new_order) = new_order else {
+                return;
+            };
 
-        new_orders.push(new_order);
-        let order = &new_order.into_expr().order;
+            let order = &new_order.into_expr().order;
 
-        txn.update_ok(
-            order,
-            Order {
-                carrier_id: Update::set(Some(input.carrier_id)),
-                ..Default::default()
-            },
-        );
-
-        let mut total_amount = 0;
-        for (line, amount) in txn.query(|rows| {
-            let ol = rows.join(OrderLine);
-            rows.filter(ol.order.eq(order));
-            rows.into_vec((&ol, &ol.amount))
-        }) {
-            total_amount += amount;
             txn.update_ok(
-                line,
-                OrderLine {
-                    delivery_d: Update::set(Some(input.delivery_d)),
+                order,
+                Order {
+                    carrier_id: Update::set(Some(input.carrier_id)),
                     ..Default::default()
                 },
             );
-        }
 
-        txn.update_ok(
-            &order.customer,
-            Customer {
-                balance: Update::add(total_amount),
-                delivery_cnt: Update::add(1),
-                ..Default::default()
-            },
-        );
-    }
-    let txn = txn.downgrade();
-    for new_order in new_orders {
-        assert!(txn.delete_ok(new_order));
+            let mut total_amount = 0;
+            for (line, amount) in txn.query(|rows| {
+                let ol = rows.join(OrderLine);
+                rows.filter(ol.order.eq(order));
+                rows.into_vec((&ol, &ol.amount))
+            }) {
+                total_amount += amount;
+                txn.update_ok(
+                    line,
+                    OrderLine {
+                        delivery_d: Update::set(Some(input.delivery_d)),
+                        ..Default::default()
+                    },
+                );
+            }
+
+            txn.update_ok(
+                &order.customer,
+                Customer {
+                    balance: Update::add(total_amount),
+                    delivery_cnt: Update::add(1),
+                    ..Default::default()
+                },
+            );
+
+            let txn = txn.downgrade();
+            assert!(txn.delete_ok(new_order));
+        })
     }
 }
