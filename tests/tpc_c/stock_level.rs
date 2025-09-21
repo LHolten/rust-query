2e87a0a6f6a5237c16b0f15a8c008cafe5ef9c0b
lucasholten <lucas@lucasholten.com>
2025-09-21 21:02:30 +0200

    Update stock_level

--- tests/tpc_c/stock_level.rs
+++ tests/tpc_c/stock_level.rs
@@ -1,30 +1,27 @@
 use super::*;
-use rust_query::{TableRow, Transaction, aggregate};
+use rust_query::{Transaction, aggregate};
 
-pub fn random_stock_level(txn: &Transaction<Schema>, district: TableRow<District>) -> i64 {
-    let input = generate_input(district);
-    stock_level(txn, input)
-}
-
-// returns the minimum quantity
-fn generate_input(district: TableRow<District>) -> StockLevelInput {
+pub fn generate_input(warehouse: i64, district: i64) -> StockLevelInput {
     StockLevelInput {
+        warehouse,
         district,
         minimum_quantity: rand::random_range(10..=20),
     }
 }
 
-struct StockLevelInput {
-    district: TableRow<District>,
+pub struct StockLevelInput {
+    warehouse: i64,
+    district: i64,
     minimum_quantity: i64,
 }
 
-fn stock_level(txn: &Transaction<Schema>, input: StockLevelInput) -> i64 {
-    let district = input.district.into_expr();
-
+pub fn stock_level(txn: &Transaction<Schema>, input: StockLevelInput) -> i64 {
     txn.query_one(aggregate(|rows| {
         let ol = rows.join(OrderLine);
-        rows.filter(ol.order.customer.district.eq(&district));
+        let district = &ol.order.customer.district;
+        rows.filter(district.warehouse.number.eq(input.warehouse));
+        rows.filter(district.number.eq(input.district));
+
         rows.filter(ol.number.gte(district.next_order.sub(20)));
         rows.filter(ol.stock.quantity.lt(input.minimum_quantity));
         rows.count_distinct(&ol.stock)
