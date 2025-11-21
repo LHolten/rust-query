63b76f4ba06a9afc183c84d745c07c4fe67bb70e
lucasholten <lucas@lucasholten.com>
2025-11-21 15:28:20 +0100

    Add documentation

--- src/async_db.rs
+++ src/async_db.rs
@@ -6,6 +6,10 @@ use std::{
 
 use crate::{Database, Transaction, migrate::Schema};
 
+/// This is an async wrapper for [Database].
+///
+/// You can easily achieve the same thing with `tokio::task::spawn_blocking`,
+/// but this wrapper is a little bit more efficient while also being runtime agnostic.
 pub struct DatabaseAsync<S> {
     inner: Arc<Database<S>>,
 }
@@ -15,6 +19,44 @@ impl<S> Clone for DatabaseAsync<S> {
         Self {
             inner: self.inner.clone(),
         }
+    }
+}
+
+impl<S: 'static + Send + Sync + Schema> DatabaseAsync<S> {
+    /// Create an async wrapper for the [Database].
+    ///
+    /// The database is wrapped in an [Arc] as it needs to be shared with any thread
+    /// executing a transaction. These threads can live longer than the future that
+    /// started the transaction.
+    ///
+    /// By accepting an [Arc], you can keep your own clone of the [Arc] and use
+    /// the database synchronously and asynchronously at the same time!
+    pub fn new(db: Arc<Database<S>>) -> Self {
+        DatabaseAsync { inner: db }
+    }
+
+    /// This is a lot like [Database::transaction], the only difference is that the async function
+    /// does not block the runtime and requires the closure to be `'static`.
+    /// The static requirement is because the future may be canceled, but the transaction can not
+    /// be canceled.
+    pub async fn transaction<R: 'static + Send>(
+        &self,
+        f: impl 'static + Send + FnOnce(&'static Transaction<S>) -> R,
+    ) -> R {
+        let db = self.inner.clone();
+        async_run(move || db.transaction_local(f)).await
+    }
+
+    /// This is a lot like [Database::transaction_mut], the only difference is that the async function
+    /// does not block the runtime and requires the closure to be `'static`.
+    /// The static requirement is because the future may be canceled, but the transaction can not
+    /// be canceled.
+    pub async fn transaction_mut<O: 'static + Send, E: 'static + Send>(
+        &self,
+        f: impl 'static + Send + FnOnce(&'static mut Transaction<S>) -> Result<O, E>,
+    ) -> Result<O, E> {
+        let db = self.inner.clone();
+        async_run(move || db.transaction_mut_local(f)).await
     }
 }
 
@@ -58,31 +100,5 @@ async fn async_run<R: 'static + Send>(f: impl 'static + Send + FnOnce() -> R) ->
     match handle.join() {
         Ok(val) => val,
         Err(err) => std::panic::resume_unwind(err),
-    }
-}
-
-impl<S: 'static + Send + Sync + Schema> DatabaseAsync<S> {
-    pub fn new(db: Arc<Database<S>>) -> Self {
-        DatabaseAsync { inner: db }
-    }
-
-    /// This is a lot like [Database::transaction], only difference is that the async function
-    /// does not block the runtime and requires the closure to be `'static`.
-    /// The static requirement is because the future may be canceled, but the transaction can not
-    /// be canceled.
-    pub async fn transaction<R: 'static + Send>(
-        &self,
-        f: impl 'static + Send + FnOnce(&'static Transaction<S>) -> R,
-    ) -> R {
-        let db = self.inner.clone();
-        async_run(move || db.transaction_local(f)).await
-    }
-
-    pub async fn transaction_mut<O: 'static + Send, E: 'static + Send>(
-        &self,
-        f: impl 'static + Send + FnOnce(&'static mut Transaction<S>) -> Result<O, E>,
-    ) -> Result<O, E> {
-        let db = self.inner.clone();
-        async_run(move || db.transaction_mut_local(f)).await
     }
 }
