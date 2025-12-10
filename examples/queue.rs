5e19af7682f68ebcbe60b8c958bbfc3afd932c5c
lucasholten <lucas@lucasholten.com>
2025-12-10 20:23:24 +0100

    Test queue example

--- examples/queue.rs
+++ examples/queue.rs
@@ -15,13 +15,33 @@ use v0::*;
 fn main() {
     let database = Database::new(Config::open_in_memory());
 
-    database.transaction(|txn| {
-        txn.query_one(aggregate(|rows| {
-            let queue = rows.join(Queue.typ(0));
-            let min_seq = rows.min(&queue.seq);
-            let min_seq = rows.filter_some(min_seq);
-            rows.filter(min_seq.eq(&queue.seq));
-            rows.min(queue)
-        }));
+    let items = vec![
+        Queue { seq: 10, typ: 1 },
+        Queue { seq: 11, typ: 0 },
+        Queue { seq: 10, typ: 0 },
+    ];
+
+    database.transaction_mut_ok(|txn| {
+        for item in items {
+            txn.insert_ok(item);
+        }
+
+        let res = txn
+            .lazy(aggregate(|rows| {
+                let queue = rows.join(Queue.typ(0));
+                let min_seq = rows.min(&queue.seq);
+                let min_seq = rows.filter_some(min_seq);
+                rows.filter(min_seq.eq(&queue.seq));
+                rows.min(queue)
+            }))
+            .unwrap();
+
+        assert_eq!(res.seq, 10);
+        assert_eq!(res.typ, 0);
     });
+}
+
+#[test]
+fn run() {
+    main();
 }
