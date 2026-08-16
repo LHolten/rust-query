48fce5b5d4e3ab54866cdbec6f1545d3183d6356
lucasholten <lucas@lucasholten.com>
2026-08-16 15:37:11 +0200

    Add rename test

---
+++ tests/rename.rs
@@ -0,0 +1,36 @@
+use rust_query::{
+    Database,
+    migration::{Config, schema},
+};
+
+use crate::v0::Schema;
+
+#[schema(Schema)]
+pub mod vN {
+    use rust_query::TableRow;
+
+    #[rename("foo_bar")]
+    #[primary_key("fooo_id")]
+    pub struct Foo {
+        #[rename("bax_9000")]
+        pub bax: i64,
+    }
+    pub struct Bar {
+        pub foo: TableRow<Foo>,
+    }
+}
+
+#[test]
+fn test() {
+    let db = Database::<Schema>::new(Config::open_in_memory());
+    let conn = db.rusqlite_connection();
+    let sql: String = conn
+        .query_one(
+            "SELECT sql FROM sqlite_schema WHERE name = 'foo_bar'",
+            [],
+            |row| row.get(0),
+        )
+        .unwrap();
+
+    expect_test::expect![[r#"CREATE TABLE "foo_bar" ("fooo_id" INTEGER PRIMARY KEY, "bax_9000" INTEGER NOT NULL) STRICT"#]].assert_eq(&sql);
+}
